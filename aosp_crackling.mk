a1d9658f30190cd96341a7ef0ca2c24f65be89e9
mutluemir08 <49791081+mutluemir08@users.noreply.github.com>
2019-12-02 19:49:39 +0300

    Update and rename lineage_crackling.mk to aosp_crackling.mk

--- aosp_crackling.mk
+++ aosp_crackling.mk
@@ -16,12 +16,15 @@
 $(call inherit-product, device/wileyfox/crackling/full_crackling.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Must define platform variant before including any common things
 TARGET_BOARD_PLATFORM_VARIANT := msm8916
 
-PRODUCT_NAME := lineage_crackling
+TARGET_GAPPS_ARCH :=arm64
+TARGET_BOOT_ANIMATION_RES:=720
+
+PRODUCT_NAME := aosp_crackling
 BOARD_VENDOR := wileyfox
 PRODUCT_DEVICE := crackling
 
